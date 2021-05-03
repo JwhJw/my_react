daa52a3435992ad759eb8f2a965e07d2bf0fad49
Jwh <1448209595@qq.com>
2021-05-04 01:09:50 +0800

    2-5.组件状态

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import StudentList from "./components/StudentList"
-const appkey = "jwhjwh_1585643726753"
-async function fetchAllStudent() {
-  const students = await fetch("http://open.duyiedu.com/api/student/findAll?appkey=" + appkey).then(res => res = res.json()).then(res => res.data)
-  ReactDOM.render(<StudentList students={students} />, document.getElementById("root"))
-
-}
-fetchAllStudent()
+import Tick from "./components/Tick"
+import A from "./components/Test"
+const container = document.getElementById('root')
+// ReactDOM.render(<Tick num={10}/>,container)
+ReactDOM.render(<A></A>,container)
