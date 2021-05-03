ba9092582f3d82e7bbe4d26ce2a1341d88ffe03a
Jwh <1448209595@qq.com>
2021-05-03 15:58:14 +0800

    react2-2 finish

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import src1 from "./assest/u=1277958337,3281567864&fm=26&gp=0.jpeg"
+import src2 from "./assest/u=1577629086,1023767695&fm=26&gp=0.jpeg"
+import src3 from "./assest/u=161718715,342072046&fm=26&gp=0.jpeg"
+import src4 from "./assest/u=1934026169,3083804955&fm=26&gp=0.jpeg"
+import src5 from "./assest/1.jpeg"
+import "./index.css"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const srcList = [src1, src2, src3, src4, src5];
+const container = document.getElementById('root');
+let index = 0;
+let timer;
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function start() {
+  stop();
+  timer = setInterval(() => {
+    index = (index + 1) % srcList.length;
+    ReactDOM.render(<img src={srcList[index]} alt=""></img>, container);
+  }, 1000);
+}
+function stop() {
+  clearInterval(timer)
+}
+start();
+container.onmouseenter = function () {
+  stop();
+}
+container.onmouseleave = function () {
+  start()
+}
