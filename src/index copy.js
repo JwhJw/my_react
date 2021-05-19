0619f98b793abd5547b8f2f6a7ff9acaaf08a275
Jwh <1448209595@qq.com>
2021-05-19 08:13:03 +0800

    2-12.demo制作朦层组件

--- src/index copy.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Comp from './components/Comp'
-
-const container = document.getElementById('root')
-const content = <div>content</div>
-ReactDOM.render(<Comp content={content}>
-</Comp>, container)
--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Comp from './components/Comp'
+import Test from './components/Test'
 
 const container = document.getElementById('root')
-const content1 = <div>content1</div>
-const content2 = <div>content2</div>
-const content3 = <div>content3</div>
-ReactDOM.render(<Comp>
-    {content1}
-    {content2}
-    {content3}
-</Comp>, container)
+
+ReactDOM.render(<Test />, container)
